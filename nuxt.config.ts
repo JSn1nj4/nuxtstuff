78d6d9b6496f4677ac4909eefaf5ee4b9d60d66c
Elliot Derhay <elliot@elliotderhay.com>
2022-05-28 22:41:04 -0400

    Allow override Nuxt config via .env
    It's only a handful of settings, but it'll make local dev more flexible to the developer.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,25 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    // vite: {
-    //     server: {
-    //         https: true,
-    //         hmr: {
-    //             protocol: 'wss',
-    //         },
-    //     },
-    // },
+  vite: {
+    server: {
+      https: process.env.VITE_SERVER_HTTPS &&
+        (process.env.VITE_SERVER_HTTPS === "true"),
+      host: process.env.VITE_SERVER_HOST ?
+        (
+          process.env.VITE_SERVER_HOST !== "true" ?
+            process.env.VITE_SERVER_HOST :
+            true
+        ) :
+        'localhost',
+      hmr: {
+        host: process.env.VITE_SERVER_HMR_HOST ?
+          process.env.VITE_SERVER_HMR_HOST :
+          'localhost',
+        protocol: process.env.VITE_SERVER_HMR_PROTOCOL ?
+          process.env.VITE_SERVER_HMR_PROTOCOL :
+          'ws',
+      },
+    },
+  },
 })
